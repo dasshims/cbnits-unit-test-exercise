99f60d93b58f9ec3c5eb25e902808d9276b919a1
himangshudas <himangshu@cbnits.com>
2020-05-20 17:21:23 -0700

    Before and after classes

--- src/test/java/com/cbnits/exercise/CalculatorTest.java
+++ src/test/java/com/cbnits/exercise/CalculatorTest.java
@@ -1,13 +1,31 @@
 package com.cbnits.exercise;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
+    public Calculator calculator;
+
+    @BeforeClass
+    public static void initializeCalculator(){
+        Calculator calculator = new Calculator();
+    }
+
+    @Before
+    public static void before(){
+        System.out.println("Before test initialize resources");
+    }
+
+    @After
+    public static void after(){
+        System.out.println("After tests destroy resources");
+    }
     @Test
     public void testSum()  {
-        Calculator calculator = new Calculator();
         int expectedResult = 3;
         int actualResult = calculator.sum(1, 2);
         assertEquals(expectedResult, actualResult);
